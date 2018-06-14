7bd52e550b3e97b4e8f3adc257fbe5ad3d347946
chethanr <chethan.vasishta@gmail.com>
2018-06-14 21:07:57 +0530

    1. Adding all the header files required for constants 2. Adding a build.bat. This currently points to smatyukevich's docker. Point it to our docker when I learn to build one!

---
+++ src/include/peripherals/base.h
@@ -0,0 +1,6 @@
+#ifndef _P_BASE_H_
+#define _P_BASE_H_
+
+#define PBASE 0x3F000000
+
+#endif // _P_BASE_H_
---
+++ src/include/utils.h
@@ -0,0 +1,8 @@
+#ifndef _BOOT_H_
+#define _BOOT_H_
+
+extern void delay(unsigned long);
+extern void put32(unsigned long, unsigned int);
+extern unsigned int get32(unsigned long);
+
+#endif // _BOOT_H_
